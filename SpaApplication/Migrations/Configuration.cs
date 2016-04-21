b350a76f2396f4050006d48d1c26ce85076932fe
FormTestudo <mt_sak12@hotmail.com>
2016-04-21 14:34:54 -0600

    Second Commit

---
+++ SpaApplication/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace SpaApplication.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<SpaApplication.Models.SpaContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "SpaApplication.Models.SpaContext";
+        }
+
+        protected override void Seed(SpaApplication.Models.SpaContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
